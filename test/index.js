6b5e4df04e6fd1dc5d0cf7a041164b667626fe21
synder <yuwei@1391.com>
2016-10-21 09:29:17 +0800

    add test file

--- test/index.js
+++ test/index.js
@@ -9,11 +9,15 @@ var filter = new Filter();
 filter.append(Filter.KEYWORDS.CONTRABAND); //系统已经定义词库
 filter.append('垃圾');
 filter.append('fuck');
+filter.append('av');
 filter.append('baidu.com');
 
-var str = "这是一本垃-圾书, 可以买卖军　刀海洛因, fu（）ck, ｆｕｃｋ,ｆｕ％ｃｋ, 网址：http://www.baidu.com";
+var str = "这是一本垃-圾书, avi 可以买卖军　刀海洛因, fu你ck, ｆｕｃｋ,ｆｕ％ｃｋ, 网址：http://www.baidu.com";
 
 var results = filter.search(str);
+
+console.log(results);
+
 
 for(var i = 0; i < results.indexs.length; i++) {
     console.log(str.substr(results.indexs[i].index, results.indexs[i].length), results.indexs[i].weight);
